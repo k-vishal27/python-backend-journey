8ea9d1e50512368dffb8f764f4f5610a8336d898
k-vishal27 <vishalk.in27@gmail.com>
2025-12-19 18:47:01 +0530

    Add files via upload

--- DSA/reverse.py
+++ DSA/reverse.py
@@ -1,1 +1,28 @@
+def reverse_array(lst):
+    if len(lst) < 2:
+        return lst
 
+    left, right = 0, len(lst) - 1
+
+    while left < right:
+        lst[left], lst[right] = lst[right], lst[left]
+        left += 1
+        right -= 1
+
+    return lst
+
+
+try:
+    size = int(input("Enter size: "))
+    if size <= 0:
+        raise ValueError("Size must be positive")
+
+    lst = []
+    for i in range(size):
+        lst.append(int(input(f"Enter value [{i+1}]: ")))
+
+    print("\nOriginal array:", lst)
+    print("Reversed array:", reverse_array(lst))
+
+except ValueError as e:
+    print("Invalid input:", e)
